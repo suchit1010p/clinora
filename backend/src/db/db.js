e00cf75d50786bf8b3a3dc0cfd6efbc861826bb1
suchit1010patel <suchit@ywork.ai>
2026-06-26 16:57:00 +0530

    Db connection improved by solving future connection related issues.

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,14 +1,16 @@
 import { neon } from "@neondatabase/serverless"
 
+export const sql = neon(process.env.DATABASE_URL);
+
 const connectDB = async () => {
     try {
-        const sql = neon(process.env.DATABASE_URL)
         const result = await sql`SELECT version()`;
         if(result) {
             console.log("\nPostgreSQL is connected....!!")
         }
     } catch (error) {
         console.log(`PostgreSQL connection error: \n ${error}`)
+        throw error
     }
 }
 
